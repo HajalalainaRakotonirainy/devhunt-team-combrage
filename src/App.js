ae98ed33c66deb0487eb90505ff617e0de6151b2
HajalalainaRakoto <hajalalainarakotonirainy@gmail.com>
2023-03-01 18:07:15 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Swal from "sweetalert2";
+import { Skeleton, Alert } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
+      <Skeleton variant="rectangular" width={210} height={118} />
+      <Alert severity="success">This is a success alert — check it out!</Alert>
+      <button
+        onClick={() =>
+          Swal.fire("The Internet?", "That thing is still around?", "question")
+        }
+      >
+        test
+      </button>
     </div>
   );
 }
